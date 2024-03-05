15666bfae628e6d1f4f8a0b5c0bb02a644fd5309
samigabor <samoila.gabor@yahoo.com>
2024-03-05 18:57:29 +0200

    fix: comment out projectId for walletConnect connector, remove erroring dependencies, change app name

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -7,7 +7,7 @@ export const config = createConfig({
   connectors: [
     injected(),
     coinbaseWallet({ appName: 'Create Wagmi' }),
-    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+    // walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
   ],
   transports: {
     [mainnet.id]: http(),
